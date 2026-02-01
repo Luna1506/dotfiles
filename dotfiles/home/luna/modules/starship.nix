32950dc8a45ec11167e9abad23d8a04d0c1cc11f
Luna <mhaiplick1506@gmail.com>
2026-02-01 23:37:16 +0100

    starship styled

--- dotfiles/home/luna/modules/starship.nix
+++ dotfiles/home/luna/modules/starship.nix
@@ -3,43 +3,40 @@
   programs.starship = {
     enable = true;
     package = pkgs.starship;
-
-    # Bash init
     enableBashIntegration = true;
 
     settings = {
       add_newline = false;
 
-      # Prompt: | user@host | path | git_branch |
       format = "$username$hostname$directory$git_branch$character";
 
       username = {
         show_always = true;
-        style_user = "white";
-        format = "| [$user]($style)";
+        style_user = "bold white";
+        format = "< [$user]($style)";
       };
 
       hostname = {
         ssh_only = false;
-        style = "white";
-        format = "@[$hostname]($style) | ";
+        style = "bold white";
+        format = "@[$hostname]($style) ";
       };
 
       directory = {
-        style = "white";
-        format = "[$path]($style) | ";
+        style = "bold white";
+        format = "[$path]($style) ";
         truncation_length = 3;
         truncation_symbol = "…/";
       };
 
       git_branch = {
-        style = "white";
-        format = " [$branch]($style) | ";
+        style = "bold white";
+        format = " [$branch]($style) ";
       };
 
       character = {
-        success_symbol = "[>](white) ";
-        error_symbol = "[>](white) ";
+        success_symbol = "[>](bold white) ";
+        error_symbol = "[>](bold white) ";
       };
     };
   };
