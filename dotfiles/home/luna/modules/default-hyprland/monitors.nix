200f273aace5646f55cbc8369899c4de7033f616
Luna <mhaiplick1506@gmail.com>
2026-04-10 18:21:02 +0200

    2026-04-10 18:21:02

--- dotfiles/home/luna/modules/default-hyprland/monitors.nix
+++ dotfiles/home/luna/modules/default-hyprland/monitors.nix
@@ -1,7 +1,10 @@
 { zoom, ... }:
 {
   wayland.windowManager.hyprland.settings = {
-    monitor = [ ",preferred,auto,${zoom}" ];
+    monitor = [
+      "eDP-1,1920x1080@144,0x0,${zoom}"
+      "HDMI-A-1,2560x1440@75,1920x0,1"
+      ",preferred,auto,1"
+    ];
   };
 }
-
