db1788c0d2963e4aaacdb69ae2ad7d379a46b9f1
Luna <mhaiplick1506@gmail.com>
2026-06-01 10:47:00 +0200

    2026-06-01 10:47:00

--- dotfiles/home/luna/modules/default-hyprland/binds.nix
+++ dotfiles/home/luna/modules/default-hyprland/binds.nix
@@ -9,7 +9,7 @@
       "$mainMod, T, togglefloating"
       "$mainMod, R, exec, wofi --show drun"
       "$mainMod, P, pin"
-      "$mainMod, J, swapsplit"
+      "$mainMod, J, layoutmsg, togglesplit"
       "$mainMod, F, fullscreen"
       "$mainMod, B, exec, ~/.config/hypr/scripts/waybar-toggle.sh"
       "$mainMod, L, exec, hyprlock"
